cdfba6de31a821bf1f7debbcbf799ad767a4d083
pe5ha <pavel.naumenko.2000@yandex.ru>
2025-03-21 12:36:28 +0300

    update Utils

--- utils/Utils.js
+++ utils/Utils.js
@@ -1,65 +1,20 @@
 
-function stringDate(timestamp=null, beOnlyDate=false, gmt="GMT+3"){
-  if(beOnlyDate){
-    if(timestamp) return Utilities.formatDate(new Date(timestamp), gmt, "dd.MM.yyyy");
-    else return Utilities.formatDate(new Date(), gmt, "dd.MM.yyyy");
-  }
-  else {
-    if(timestamp) return Utilities.formatDate(new Date(timestamp), gmt, "dd.MM.yyyy HH:mm:ss");
-    else return Utilities.formatDate(new Date(), gmt, "dd.MM.yyyy HH:mm:ss");
-  }
+function stringDate(timestamp=null){
+  if(timestamp) return Utilities.formatDate(new Date(timestamp), "GMT+3", "dd.MM.yyyy HH:mm:ss");
+  else return Utilities.formatDate(new Date(), "GMT+3", "dd.MM.yyyy HH:mm:ss");
 }
 
-function stringDateV2(date, beOnlyDate=false, gmt="GMT+3"){
-  if(beOnlyDate){
-    return Utilities.formatDate(date, gmt, "dd.MM.yyyy");
-  }
-  else {
-    return Utilities.formatDate(date, gmt, "dd.MM.yyyy HH:mm:ss");
-  }
-}
-function stringDateDash(date,beOnlyDate=false, gmt="GMT+3"){
-  if(beOnlyDate){
-    return Utilities.formatDate(date, gmt, "yyyy-MM-dd");
-  }
-  else {
-    // return Utilities.formatDate(date, gmt, "yyyy-MM-dd HH:mm:ss");
-  }
-}
-
-function getMonthNameByDate(date){
-  return monthNames[date.getMonth()];
-}
-
-const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
-
+/**
+ * Поиск первой пустой строки в диапазоне (по первому столбцу в диапазоне)
+ * @param {*} range 
+ * @returns 
+ */
 function getFirstEmptyRow(range){
   let data = range.getValues();
   for(let i=0;i<data.length;i++){
     if(data[i][0]==="") return i+1;
   }
   return -1;
-}
-
-function getFirstEmptyRowInAA(sheet){
-  let data = sheet.getRange("A:A").getValues();
-  for(let i=0;i<data.length;i++){
-    if(data[i][0]==="") return i+1;
-  }
-  sheet.insertRowAfter(sheet.getLastRow());
-  return sheet.getLastRow()+1;
-}
-
-function isValidHttpUrl(string) {
-  let url;
-  try {
-    url = new URL(string);
-  } catch (_) {
-    return false;
-  }
-  return url.protocol === "http:" || url.protocol === "https:";
 }
 
 function findRowIn2dRange(array,column,value){
@@ -71,14 +26,12 @@ function findRowIn2dRange(array,column,value){
   return -1;
 }
 
-function findRowIn2dRangeString(array,column,value){
-  for (let i = 0; i < array.length; i++) { 
-    if (String(array[i][column]) == String(value)) {
-      return i;
-    }
+function isValidHttpUrl(string) {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
   }
-  return -1;
+  return url.protocol === "http:" || url.protocol === "https:";
 }
-
-
-
